d989912b348183cfb01cb13d2d15f39fd2a93c37
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-06-23 14:03:01 +0430

    Add testcases for clear all function

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,51 +1,46 @@
 // Function to add a task to the list
-function addTask(taskDescription, tasks){
+function addTask(taskDescription, tasks) {
   const task = {
     description: taskDescription,
     completed: false,
     id: tasks.length,
   };
   tasks.push(task);
-};
-<<<<<<< HEAD
+}
 
-// Function to remove completed tasks from the list
-function removeTask(taskId, tasks){
+// Function to remove a task from the list
+function removeTask(taskId, tasks) {
   const taskIndex = tasks.findIndex((task) => task.id === taskId);
-  if (tasks.id !== -1) {
+  if (taskIndex !== -1) {
     tasks.splice(taskIndex, 1);
   }
-};
+}
 
-// Function for editing 
-function editTaskDescription(taskId, newDescription) {
-  const tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+// Function to edit the description of a task
+function editTaskDescription(taskId, newDescription, tasks) {
   const task = tasks.find((task) => task.id === taskId);
   if (task) {
     task.description = newDescription;
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    return task;
   }
-  return null;
 }
-=======
->>>>>>> 6693f631f42b2c526b88d54d0a1211cd57a9bb95
 
-// Function to remove completed tasks from the list
-function removeTask(taskId, tasks){
-  const taskIndex = tasks.findIndex((task) => task.id === taskId);
-  if (tasks.id !== -1) {
-    tasks.splice(taskIndex, 1);
+// Function to update an item's 'completed' status
+function updateTaskStatus(taskId, completed, tasks) {
+  const task = tasks.find((task) => task.id === taskId);
+  if (task) {
+    task.completed = completed;
   }
-};
+}
 
+// Function to clear all completed tasks
+function clearCompletedTasks(tasks) {
+  tasks = tasks.filter((task) => !task.completed);
+  return tasks; // Return the filtered tasks array
+}
 module.exports = {
   addTask,
   removeTask,
-<<<<<<< HEAD
   editTaskDescription,
   updateTaskStatus,
   clearCompletedTasks,
-=======
->>>>>>> 6693f631f42b2c526b88d54d0a1211cd57a9bb95
 };
